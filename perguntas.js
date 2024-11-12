e1bc0bd3df442224bce2d01edc4b5a6bd4b8b701
melanyakemi <melany.ono@escola.pr.gov.br>
2024-11-12 11:49:05 -0300

    YJKWO

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'educação fisica',
+    'QUIZIZZ',
     'quantos jogadores participam em uma partida de futsal?',
     'duas equipes com 5 atletas'
 )
 
 criaCartao(
-    'educação fisica',
+    'QUIZIZZ',
     'quanto tempo dura uma partida de futsal?',
     '40 minutos'
 )
 
 criaCartao(
-    'educação fisica',
+    'QUIZIZZ',
     'quais são os cartões usados no futsal?',
     'cartões amarelos ou vermelhos'
 )
 
 criaCartao(
-    'educação fisica',
+    'QUIZIZZ',
     'Quanto vale o wo?',
     'imposição da perda da partida pelo placar de 3 x 0 (três a zero) ao Clube que der causa à sua não realização ou continuidade.'
 )
