8e862c4fca28f800ae1cf6921f3441cd3b5a5d55
melanyakemi <melany.ono@escola.pr.gov.br>
2024-11-12 11:47:59 -0300

    u6wjwji

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'educação fisica',
+    'quantos jogadores participam em uma partida de futsal?',
+    'duas equipes com 5 atletas'
+)
+
+criaCartao(
+    'educação fisica',
+    'quanto tempo dura uma partida de futsal?',
+    '40 minutos'
+)
+
+criaCartao(
+    'educação fisica',
+    'quais são os cartões usados no futsal?',
+    'cartões amarelos ou vermelhos'
+)
+
+criaCartao(
+    'educação fisica',
+    'Quanto vale o wo?',
+    'imposição da perda da partida pelo placar de 3 x 0 (três a zero) ao Clube que der causa à sua não realização ou continuidade.'
+)
